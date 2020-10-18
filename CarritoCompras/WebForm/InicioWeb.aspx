13226061a2c213e95256b9eff432bb913a9fb796
hernanVillalba <63215609+hernanVillalba@users.noreply.github.com>
2020-10-18 17:12:48 -0300

    subida proyecto web desde cero

---
+++ CarritoCompras/WebForm/InicioWeb.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="InicioWeb.aspx.cs" Inherits="WebForm.InicioWeb" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView ID="dgvArticulos" runat="server"></asp:GridView>
+</asp:Content>
---
+++ CarritoCompras/WebForm/InicioWeb.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
+
+namespace WebForm
+{
+    public partial class InicioWeb : System.Web.UI.Page
+    {
+        List<Articulos> Lista = new List<Articulos>(); 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //datagriedview
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            Lista = negocio.Listar();
+            dgvArticulos.DataSource = Lista;
+            dgvArticulos.DataBind();
+
+
+        }
+    }
+}
